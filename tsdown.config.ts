9cd828c4c298776945f9ceace3880bb7ef030434
Miyana Stange <mia.stange@outlook.com>
2026-03-13 13:25:42 +0100

    feat: added tsdown config

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['src/client.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'dist/lib',
+});
