49d6e7e0cd0d497e21ffe6a54b7705680d6ed7fc
Miyana Stange <mia.stange@outlook.com>
2026-06-03 15:50:35 +0200

    feat: updated circuit breaker config, added errorFilter

--- src/lib/conf/circuitbreaker_config.ts
+++ src/lib/conf/circuitbreaker_config.ts
@@ -1,13 +1,22 @@
 export const defaultCircuitConfig = {
   enabled: true,
   name: 'crypto-grpc',
-  maxRequests: 3,
-  interval: 30,
-  timeout: 5,
-  consecutiveFailures: 3,
+  rollingCountTimeout: 120000,
+  timeout: 30000,
+  errorThresholdPercentage: 25,
+  resetTimeout: 5000,
   failureStatusCodes: [
     14, // UNAVAILABLE
     8, // RESOURCE_EXHAUSTED
     10, // ABORTED
   ],
+  errorFilter: (err) => {
+    // consider only provided failureStatusCodes as errors (i.e. return false)
+    return (
+      typeof err === 'object' &&
+      'code' in err &&
+      typeof err.code === 'number' &&
+      !defaultCircuitConfig.failureStatusCodes.includes(err.code)
+    );
+  },
 };
